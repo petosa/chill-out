f04ac4ee0f8e27ee487dd29715b6e59dd73f04c4
poetienshul <ethantien@hotmail.com>
2019-10-01 19:59:15 -0400

    Implemented gradual unfreezing

--- alexnet.py
+++ alexnet.py
@@ -15,29 +15,29 @@ class AlexNet(nn.Module):
     def __init__(self, num_classes=1000):
         super(AlexNet, self).__init__()
         self.features = nn.Sequential(
-            nn.Conv2d(3, 64, kernel_size=11, stride=4, padding=2),
-            nn.ReLU(inplace=True),
-            nn.MaxPool2d(kernel_size=3, stride=2),
-            nn.Conv2d(64, 192, kernel_size=5, padding=2),
-            nn.ReLU(inplace=True),
-            nn.MaxPool2d(kernel_size=3, stride=2),
-            nn.Conv2d(192, 384, kernel_size=3, padding=1),
-            nn.ReLU(inplace=True),
-            nn.Conv2d(384, 256, kernel_size=3, padding=1),
-            nn.ReLU(inplace=True),
-            nn.Conv2d(256, 256, kernel_size=3, padding=1),
-            nn.ReLU(inplace=True),
-            nn.MaxPool2d(kernel_size=3, stride=2),
+            nn.Conv2d(3, 64, kernel_size=11, stride=4, padding=2), # 0
+            nn.ReLU(inplace=True), # 1
+            nn.MaxPool2d(kernel_size=3, stride=2), # 2
+            nn.Conv2d(64, 192, kernel_size=5, padding=2), # 3
+            nn.ReLU(inplace=True), # 4
+            nn.MaxPool2d(kernel_size=3, stride=2), # 5
+            nn.Conv2d(192, 384, kernel_size=3, padding=1), # 6
+            nn.ReLU(inplace=True), # 7
+            nn.Conv2d(384, 256, kernel_size=3, padding=1), # 8
+            nn.ReLU(inplace=True), # 9
+            nn.Conv2d(256, 256, kernel_size=3, padding=1), # 10
+            nn.ReLU(inplace=True), # 11
+            nn.MaxPool2d(kernel_size=3, stride=2), # 12
         )
-        self.avgpool = nn.AdaptiveAvgPool2d((6, 6))
+        self.avgpool = nn.AdaptiveAvgPool2d((6, 6)) #13
         self.classifier = nn.Sequential(
-            nn.Dropout(),
-            nn.Linear(256 * 6 * 6, 4096),
-            nn.ReLU(inplace=True),
-            nn.Dropout(),
-            nn.Linear(4096, 4096),
-            nn.ReLU(inplace=True),
-            nn.Linear(4096, num_classes),
+            nn.Dropout(), # 0
+            nn.Linear(256 * 6 * 6, 4096), # 1
+            nn.ReLU(inplace=True), # 2
+            nn.Dropout(), # 3 
+            nn.Linear(4096, 4096), # 4
+            nn.ReLU(inplace=True), # 5
+            nn.Linear(4096, num_classes), # 6
         )
 
     def forward(self, x):
@@ -48,7 +48,7 @@ class AlexNet(nn.Module):
         return x
 
 
-def alexnet(pretrained=False, progress=True, **kwargs):
+def alexnet(pretrained=False, progress=True, num_classes=10, **kwargs):
     r"""AlexNet model architecture from the
     `"One weird trick..." <https://arxiv.org/abs/1404.5997>`_ paper.
 
@@ -61,4 +61,8 @@ def alexnet(pretrained=False, progress=True, **kwargs):
         state_dict = load_state_dict_from_url(model_urls['alexnet'],
                                               progress=progress)
         model.load_state_dict(state_dict)
+    
+    #set_parameter_requires_grad(model_ft, feature_extract)
+    model.classifier[6] = nn.Linear(model.classifier[6].in_features,num_classes)
+    input_size = 224
     return model
---
+++ train.py
@@ -0,0 +1,128 @@
+import alexnet
+import torch
+from torch import nn
+from torch.autograd import Variable
+import torch.nn.functional as F
+import torch.optim as optim
+import torchvision
+import numpy as np
+import argparse
+from torch.utils.data.sampler import SubsetRandomSampler
+
+parser = argparse.ArgumentParser(description='Chill-out')
+# Hyperparameters
+parser.add_argument('--lr', type=float, metavar='LR', default=1e-4,
+                    help='learning rate')
+parser.add_argument('--momentum', type=float, metavar='M', default=.9,
+                    help='SGD momentum')
+parser.add_argument('--weight-decay', type=float, default=0.0,
+                    help='Weight decay hyperparameter')
+parser.add_argument('--batch-size', type=int, metavar='N', default=256,
+                    help='input batch size for training')
+parser.add_argument('--epochs', type=int, metavar='N', default=10,
+                    help='number of epochs to train')
+parser.add_argument('--model',
+                    choices=['alexnet'],
+                    help='which model to train/evaluate')
+parser.add_argument('--hidden-dim', type=int,
+                    help='number of hidden features/activations')
+parser.add_argument('--kernel-size', type=int,
+                    help='size of convolution kernels/filters')
+parser.add_argument('--epochs-between-states', type=int, help='idfk')
+parser.add_argument('--val-percentage', type=float, default=.2, help='idfk')
+# Other configuration
+parser.add_argument('--no-cuda', action='store_true', default=False,
+                    help='disables CUDA training')
+parser.add_argument('--seed', type=int, default=1, metavar='S',
+                    help='random seed (default: 1)')
+parser.add_argument('--test-batch-size', type=int, default=512, metavar='N',
+                    help='input batch size for testing (default: 1000)')
+parser.add_argument('--log-interval', type=int, default=10, metavar='N',
+                    help='number of batches between logging train status')
+parser.add_argument('--cifar10-dir', default='data',
+                    help='directory that contains cifar-10-batches-py/ '
+                         '(downloaded automatically if necessary)')
+args = parser.parse_args()
+args.cuda = not args.no_cuda and torch.cuda.is_available()
+torch.manual_seed(args.seed)
+if args.cuda:
+    torch.cuda.manual_seed(args.seed)
+
+#Load data
+transform = torchvision.transforms.Compose(
+    [torchvision.transforms.Resize((224, 224)),
+     torchvision.transforms.ToTensor(),
+     torchvision.transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
+    ])
+
+trainset = torchvision.datasets.CIFAR10(args.cifar10_dir, train=True,
+                                        download=True, transform=transform)
+
+idx = list(range(len(trainset)))
+np.random.shuffle(idx)
+val_percentage = args.val_percentage
+split_idx = int(val_percentage*len(trainset))
+val_idx = idx[-split_idx:]
+train_idx = idx[:-split_idx]
+
+train_sampler = SubsetRandomSampler(train_idx)
+val_sampler = SubsetRandomSampler(val_idx)
+
+train_loader = torch.utils.data.DataLoader(
+    trainset, batch_size=args.batch_size, sampler=train_sampler,
+    num_workers=2, pin_memory=True,
+)
+val_loader = torch.utils.data.DataLoader(
+    trainset, batch_size=args.batch_size, sampler=val_sampler,
+    num_workers=2, pin_memory=True,
+)
+
+testset = torchvision.datasets.CIFAR10(args.cifar10_dir, train=False,
+                                       download=True, transform=transform)
+
+test_loader = torch.utils.data.DataLoader(
+    testset, batch_size=args.batch_size, shuffle=True,
+    num_workers=2, pin_memory=True,
+)
+
+classes = ('plane', 'car', 'bird', 'cat',
+           'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
+n_classes = 10
+
+
+model = alexnet.alexnet(pretrained=True)
+
+# gradual unfreezing
+model.train()
+criterion = F.cross_entropy
+optimizer = optim.SGD(model.parameters(), lr=args.lr, momentum=args.momentum, weight_decay=args.weight_decay)
+policy = [[False]*(8-i) + [True]*i for i in range(1,9)]
+# policy = [
+#     [False]*7 + [True],
+#     [False]*6 + [True]*2,
+#     [False]*5 + [True]*3,
+#     [False]*4 + [True]*4,
+#     [False]*3 + [True]*5,
+#     [False]*2 + [True]*6,
+#     [False]*1 + [True]*7,
+#     [True]*8,
+# ]
+print (model)
+for step in policy:
+    # model.named_parameters = [layer1.weight, layer1.bias, layer2.weight....]
+    for i, (layer, w) in enumerate(model.named_parameters()):
+        w.requires_grad = step[i // 2] # weight and bias are in named_parameters, not in policy
+    print ("Current Policy : " + str(step))
+    for i in range(args.epochs):
+        for batch_idx, batch in enumerate(train_loader):
+            images, targets = Variable(batch[0]), Variable(batch[1])
+            if args.cuda:
+                images, targets = images.cuda(), targets.cuda()
+            model.zero_grad()
+            output = model(images)
+            loss = criterion(output, targets)
+            loss.backward()
+            optimizer.step()
+        print ("Epoch : " + str(i))
+        
+torch.save(model.state_dict(), 'model.pt')
