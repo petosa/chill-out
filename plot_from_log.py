a4ef14cd855aff76c0f240c4765d57fd9dad2156
poetienshul <ethantien@hotmail.com>
2019-10-04 22:40:10 -0400

    plotting scripts

---
+++ plot_from_log.py
@@ -0,0 +1,49 @@
+import re
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+
+regex = r'[0-9]+\,[0-9]*\.?[0-9]+\,[0-9]+\,[0-9]*\.?[0-9]+\,[0-9]+'
+loss_line = re.compile(regex)
+
+files = ['gradual_unfreezing.log', 'chain_thaw.log', 'unfreeze_all.log']
+
+fig = plt.figure()
+
+for i, filename in enumerate(files):
+    train_losses = []
+    train_accs = []
+    val_losses = []
+    val_accs = []
+
+    with open('logs/' + filename, 'r') as f:
+        for line in f:
+            if loss_line.match(line):
+                _, train_loss, train_acc, valid_loss, valid_acc = line.strip().split(',')
+                train_losses.append(train_loss)
+                train_accs.append(train_acc)
+                val_losses.append(valid_loss)
+                val_accs.append(valid_acc)
+    plt.plot(val_losses, label=filename.split('.')[0])
+
+plt.title('All Loss')
+plt.ylabel('loss')
+plt.ylim((0, 1))
+plt.legend(loc = 'upper left')
+fig.savefig('all_loss.png')
+
+# plt.plot(train_losses, label='train', color='blue')
+# plt.plot(val_losses, label='val', color='orange')
+# plt.title('Chain Thaw All Loss')
+# plt.ylabel('loss')
+# plt.ylim((0, 1))
+# plt.legend(loc = 'upper left')
+# fig.savefig('all_loss.png')
+
+# fig = plt.figure()
+# plt.plot(train_accs, label='train', color='blue')
+# plt.plot(val_accs, label='val', color='orange')
+# plt.title('Chain Thaw All Accuracy')
+# plt.ylabel('accuracy')
+# plt.legend(loc = 'lower left')
+# fig.savefig('all_acc.png')
