0088813c9cdee8f75e24b14355f456ef794de66d
Nick Petosa <nick.petosa@gmail.com>
2019-10-30 21:51:18 -0400

    eval cli

--- evaluate_test.py
+++ evaluate_test.py
@@ -1,44 +1,20 @@
 import train
 import util
 import torch
+import sys
+import os
+import shutil
 
-
+session, ckpt = sys.argv[1].split("/")
+session = int(session)
+ckpt = int(ckpt.split(".")[0])
 print("Custom")
-t= train.Trainer(0, None)
+t = train.Trainer(0, None)
 m = util.load_alexnet(10).cuda()
 o = torch.optim.SGD(m.parameters(),1e-4,.9,weight_decay=1e-4,nesterov=True)
-util.full_load(m,o,133,1572362438)
+util.full_load(m,o,ckpt,session)
 
 print(t.evaluate(m, "train"))
 print(t.evaluate(m, "val"))
 print(t.evaluate(m, "test"))
-
-
-print("Unfreezing")
-m = util.load_alexnet(10).cuda()
-o = torch.optim.SGD(m.parameters(),1e-4,.9,weight_decay=1e-4,nesterov=True)
-util.full_load(m,o,9,1572359480)
-
-print(t.evaluate(m, "train"))
-print(t.evaluate(m, "val"))
-print(t.evaluate(m, "test"))
-
-
-print("Gradual unfreeze")
-m = util.load_alexnet(10).cuda()
-o = torch.optim.SGD(m.parameters(),1e-4,.9,weight_decay=1e-4,nesterov=True)
-util.full_load(m,o,8,1572359851)
-
-print(t.evaluate(m, "train"))
-print(t.evaluate(m, "val"))
-print(t.evaluate(m, "test"))
-
-
-print("Chain thaw")
-m = util.load_alexnet(10).cuda()
-o = torch.optim.SGD(m.parameters(),1e-4,.9,weight_decay=1e-4,nesterov=True)
-util.full_load(m,o,9,1572360442)
-
-print(t.evaluate(m, "train"))
-print(t.evaluate(m, "val"))
-print(t.evaluate(m, "test"))
+shutil.rmtree("0")
