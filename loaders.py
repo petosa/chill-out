5f3db955950299aef3ce025e70cd1b8a5d7853ad
Chaitya Shah <chaityabshah@verizon.net>
2019-11-24 17:36:59 -0500

    scene loader

--- loaders.py
+++ loaders.py
@@ -50,3 +50,51 @@ def load_cifar(train_size, val_size, batch_size=256, data_folder="data", verbose
     )
     criterion = F.cross_entropy
     return train_loader, val_loader, test_loader, criterion
+
+
+def load_scenes(train_size, val_size, batch_size=256, data_folder="data", verbose=True):
+
+    transform = torchvision.transforms.Compose([
+        torchvision.transforms.Resize((224, 224)),
+        torchvision.transforms.ToTensor(),
+        torchvision.transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
+    ])
+
+    datapath = os.path.join(data_folder, "scene15")
+    dataset = torchvision.datasets.ImageFolder(root=datapath, transform=transform)
+    idx = list(range(len(dataset)))
+    np.random.shuffle(idx)
+
+    train_size = int(train_size*len(idx)) if train_size < 1 else train_size
+    val_size = int(val_size*len(idx)) if val_size < 1 else val_size
+    test_size = len(dataset)-train_size-val_size
+    
+    assert(train_size+val_size <= len(idx))
+    assert(test_size > 0)
+    if verbose:
+        print("Train size:", train_size, "Val size:", val_size, "Test size:", test_size)
+
+    train_idx = idx[:train_size]
+    val_idx = idx[train_size:train_size+val_size]
+    test_idx = idx[-test_size]
+    if verbose:
+        print("Train size:", len(train_idx), "Val size:", len(val_idx), "Test size:", len(test_idx))
+
+    train_sampler = SubsetRandomSampler(train_idx)
+    val_sampler = SubsetRandomSampler(val_idx)
+    test_sampler = SubsetRandomSampler(test_idx)
+
+    train_loader = torch.utils.data.DataLoader(
+        dataset, batch_size=batch_size, sampler=train_sampler,
+        num_workers=4, pin_memory=True
+    )
+    val_loader = torch.utils.data.DataLoader(
+        dataset, batch_size=batch_size, sampler=val_sampler,
+        num_workers=4, pin_memory=True
+    )
+    test_loader = torch.utils.data.DataLoader(
+        dataset, batch_size=batch_size, shuffle=test_sampler,
+        num_workers=4, pin_memory=True
+    )
+    criterion = F.cross_entropy
+    return train_loader, val_loader, test_loader, criterion
